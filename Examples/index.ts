7a479d94d45cf07db7ebc101b9c2599e67701c97
Cihan <cihan.ozturk@hubbox.io>
2021-08-13 15:23:00 +0300

    Ilk Başlangıç

---
+++ Examples/index.ts
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2021. Chy Bilgisayar Bilisim
+ * Author: Cihan Ozturk
+ * E-mail: cihan@chy.com.tr
+ * Github:https://github.com/cihan53/
+ */
+
+
+import Chyz from "../Chyz";
+import {User} from "../web/User";
+import {User as Identity } from "./Models/User";
+// @ts-ignore
+import {DbConnection} from "../base/DbConnection";
+
+
+let config = {
+    components: {
+        db:{
+            class:DbConnection,
+            database: process.env.DBDATABASE,
+            username:process.env.DBUSER,
+            password:process.env.DBPASS,
+            options:{
+                host: process.env.DBHOST,
+                dialect: 'postgres',  /* one of 'mysql' | 'mariadb' | 'postgres' | 'mssql' */
+                // disable logging; default: console.log
+                logging: false
+            }
+        },
+        user: {
+            'class': User,
+            'identityClass':Identity
+        }
+    }
+}
+Chyz.app(config).Start();
