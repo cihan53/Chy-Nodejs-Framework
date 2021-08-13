b65c2c6a2c3edfc39f8d2d8b3a59ee776cae5d4b
Cihan <cihan.ozturk@hubbox.io>
2021-08-13 15:23:00 +0300

    Ilk Başlangıç

---
+++ Examples/Controllers/SiteController.ts
@@ -0,0 +1,95 @@
+/*
+ * Copyright (c) 2021. Chy Bilgisayar Bilisim
+ * Author: Cihan Ozturk
+ * E-mail: cihan@chy.com.tr
+ * Github:https://github.com/cihan53/
+ */
+
+import {Controller} from "../../base/Controller";
+import BaseChyz from "../../BaseChyz";
+// @ts-ignore
+import {Request, Response} from "express";
+import {get} from "../../decorator/get";
+import {post} from "../../decorator/post";
+import {controller} from "../../decorator/controller";
+import {AccessControl} from "../../filters/AccessControl";
+import {JwtHttpBearerAuth} from "../../filters/auth/JwtHttpBearerAuth";
+import Utils from "../../requiments/Utils";
+
+@controller("/site")
+export class SiteController extends Controller {
+
+    public myCheck(token) {
+        console.log("myyyyyyyyyyyyyyyyyyyyy")
+    }
+
+    public behaviors(): any[] {
+
+        return [{
+            'authenticator': {
+                "class": JwtHttpBearerAuth,
+                // "auth": this.myCheck
+            },
+            'access': {
+                'class': AccessControl,
+                'only': ['login', 'logout', 'signup'],
+                'rules': [
+                    {
+                        'allow': true,
+                        'actions': ['login', 'index'],
+                        'roles': ['?'],
+                    },
+                    {
+                        'allow': true,
+                        'actions': ['logout', "logout2"],
+                        'roles': ['@'],
+                    }
+                ]
+            }
+        }]
+    }
+
+    @get("/")
+    Index(req: Request, res: Response) {
+
+        BaseChyz.logs().info("Site Controller Burası", this.id)
+        return res.json({message: "index sayfası"})
+    }
+
+    @post("login")
+    Login(req: Request, res: Response) {
+        BaseChyz.logs().info("Post Controller")
+        return res.send("Post Controller")
+    }
+
+    @get("logout")
+    logout(req: Request, res: Response) {
+
+        // @ts-ignore
+        let identity = req.user ?? BaseChyz.getComponent("user").getIdentity();
+        // console.log("logout2", identity.id)
+        console.log(identity)
+
+        BaseChyz.logs().info("Logout Controller")
+        return res.send("Logout Controller")
+    }
+
+    @get("logout2")
+    logout2(req: Request, res: Response) {
+
+
+        Utils.sleep(5);
+
+        BaseChyz.logs().info("Logout Controller")
+
+
+        let identity = BaseChyz.getComponent("user").getIdentity();
+        console.log("logout2", identity.id)
+        return res.send("Logout Controller")
+    }
+
+    error(req: Request, res: Response) {
+        BaseChyz.logs().info("Error Sayfası")
+        return res.send("Post Controller")
+    }
+}
---
+++ Examples/Models/User.ts
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) 2021. Chy Bilgisayar Bilisim
+ * Author: Cihan Ozturk
+ * E-mail: cihan@chy.com.tr
+ * Github:https://github.com/cihan53/
+ */
+
+import {IdentityInterface} from "../../web/IdentityInterface";
+// @ts-ignore
+import {DataTypes, Model} from "sequelize";
+import BaseChyz from "../../BaseChyz";
+
+export class User implements IdentityInterface {
+
+    private _model  ;
+
+    constructor() {
+        const sequelize = BaseChyz.getComponent("db").db;
+        this._model = sequelize.define('User', {
+            // Model attributes are defined here
+            username: {
+                type: DataTypes.STRING,
+                allowNull: false
+            },
+            password: {
+                type: DataTypes.STRING,
+                allowNull: false
+            },
+            user_role: {
+                type: DataTypes.STRING,
+                allowNull: false
+            },
+            salt_text: {
+                type: DataTypes.STRING
+                // allowNull defaults to true
+            }
+        }, {
+            tableName: 'users',
+            timestamps: false
+        });
+    }
+
+
+    get model(): any {
+        return this._model;
+    }
+
+    findIdentity(id) {
+    }
+
+    async findIdentityByAccessToken(token, type) {
+         let identity = await this._model.findOne({where: {salt_text: token.signature}});
+         return identity;
+    }
+
+    getAuthKey() {
+    }
+
+    getId() {
+    }
+
+    validateAuthKey($authKey) {
+    }
+
+
+}
+
+
