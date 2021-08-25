1c5e4f162ecb12af3ab067a2ec676e0f617295c2
Cihan <cihan.ozturk@hubbox.io>
2021-08-25 16:44:00 +0300

    Model Transaction düzenleme

--- index.ts
+++ index.ts
@@ -3,11 +3,12 @@ export * from "./base";
 export * from "./filters";
 export * from "./filters/auth";
 export * from "./decorator";
-export * from "./BaseChyz";
 export * from "./requiments/Utils";
 export {RouteDefinition} from "./model/RouteDefinition";
+export {User} from "./web/User";
 
 import BaseChyz from "./BaseChyz";
-const Chyz = new BaseChyz();
+export {BaseChyz}
 
+const Chyz = new BaseChyz();
 export default Chyz;
